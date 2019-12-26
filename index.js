0c37a91bdea5328b270e4d3879f92a8a272deea5
mkkravchenkoo <troyany2016@gmail.com>
2019-12-26 16:33:14 +0200

    connect to mongoDb

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 3001
+const dotenv = require('dotenv');
+dotenv.config({path:'./config/config.env'});
+
 
 app.get('/', (req, res) => res.send('Server is working'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const start = async () => {
+	try {
+		const rr = await mongoose.connect( process.env.MONGO_URI, {useNewUrlParser: true});
+
+		app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+		app.get('/users', async (req, res) => {
+			const result = await rr.connection.db.collection('users').find().toArray();
+			res.json(result)
+		})
+
+	}catch (e) {
+		console.log(e.message);
+		process.exit(1)
+	}
+
+}
+
+start();
