667099ac46ea496ed7386ceb41f0c4c4742e8329
troyany2016 <troyany2016@gmail.com>
2019-12-26 12:52:17 +0200

    heroku stuff

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
+
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Server is working'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
